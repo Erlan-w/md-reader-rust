65105641b9255c53c30f9fcd7fd94c465b9a6295
Erlan27 <erlanwahyu95@gmail.com>
2026-06-02 19:56:49 +0700

    penambahan scope embed_resource khusus untuk windows

--- build.rs
+++ build.rs
@@ -1,7 +1,4 @@
 fn main() {
-    // Tambah embed_resource::NONE sebagai argumen kedua
-    // Tambah .manifest_optional().unwrap() karena return type adalah #[must_use]
-    embed_resource::compile("app.rc", embed_resource::NONE)
-        .manifest_optional()
-        .unwrap();
+    #[cfg(windows)]
+    embed_resource::compile("app.rc", embed_resource::NONE);
 }
